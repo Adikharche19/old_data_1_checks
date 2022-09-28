15e0457f88dd9f0fa4271af52b86876465e76335
Adikharche19 <adikharche19@gmail.com>
2022-09-28 15:01:24 -0400

    add BMI_calculator.py

--- BMI_calculator.py
+++ BMI_calculator.py
@@ -1,15 +1,17 @@
 height = float(input("Enter the height in cm: "))  
 weight = float(input("Enter the weight in kg: "))  
+age = int(input("Enter the age: "))
 #height = 150
-#weight=50
+#weight=50.2
 bmi = weight/(height/100)**2
 print("Your body mass index is",bmi)
 if bmi <= 25:
         bmidict = {
                     "weight": weight,
-                    "Height": height
+                    "Height": height,
+                    "age": age,
+                    "Result": "You are Healthy"
                   }
-        print(bmidict)
-        print("You are Healthy")
+        print(bmidict["Result"])
 else:
         print("You are obese")
--- test.py
+++
@@ -1,8 +0,0 @@
-height = float(input("Enter the height in cm: ")) 
-
-bmidict = {
-            "weight": "underweight",
-             "health_tip": "Need workout",
-             "Height": height
-          }
-print(bmidict)
