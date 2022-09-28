bf076834115cc3ef48f8b5ebaaface08469d9adf
Adikharche19 <adikharche19@gmail.com>
2022-09-28 17:44:40 -0400

    modified feqw lines

--- list.py
+++ list.py
@@ -1,3 +1,9 @@
+#Program to calculate BMI using lists and different data types
+#Author = Aditi Kharche
+#Date = september 28,2022
+
+
+
 height = float(input("Enter the height in cm: "))  
 weight = float(input("Enter the weight in kg: "))  
 age = int(input("Enter the age: "))
@@ -6,8 +12,9 @@ age = int(input("Enter the age: "))
 bmi = weight/(height/100)**2
 print("Your body mass index is",bmi)
 if bmi <= 25:
-    bmilist = ["150","70.6","34","You are Healthy"]
-    print(bmilist[3])
+    bmilist = [height,weight,age,"You are Healthy"]
+    print("Height=" ,bmilist[0], "Weight=",bmilist[1],"Age is",bmilist[2])
+    print("Result=",bmilist[3])
 else:
     print("You are obese")
     
